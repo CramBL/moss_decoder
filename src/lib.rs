ab64c097e67b30ebe5fd448bc61d2a5279945e6e
CramBL <marcbkonig@gmail.com>
2023-07-21 15:52:20 +0200

    provide alternative multiple events decoding function

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ use moss_protocol::MossWord;
 fn moss_decoder(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(decode_event, m)?)?;
     m.add_function(wrap_pyfunction!(decode_multiple_events, m)?)?;
+    m.add_function(wrap_pyfunction!(decode_multiple_events_alt, m)?)?;
 
     m.add_class::<MossHit>()?;
     m.add_class::<MossPacket>()?;
@@ -99,6 +100,97 @@ fn decode_multiple_events(mut bytes: Vec<u8>) -> PyResult<Vec<MossPacket>> {
         Err(PyTypeError::new_err("No MOSS Packets in events"))
     } else {
         Ok(moss_packets)
+    }
+}
+
+/// Decodes multiple MOSS events into a list of [MossPacket]s
+#[pyfunction]
+fn decode_multiple_events_alt(bytes: &[u8]) -> PyResult<(Vec<MossPacket>, usize)> {
+    let byte_cnt = bytes.len();
+
+    if byte_cnt < 6 {
+        return Err(PyTypeError::new_err(
+            "Received less than the minimum event size",
+        ));
+    }
+
+    let mut moss_packets: Vec<MossPacket> = Vec::with_capacity(byte_cnt / 1024);
+
+    let mut last_trailer_idx = 0;
+
+    let mut is_moss_packet = false;
+    //let mut current_unit_id = 0xff; // placeholder
+    let mut current_region: u8 = 0xff; // placeholder
+
+    for (i, byte) in bytes.iter().enumerate() {
+        if let Ok(word) = MossWord::from_byte(*byte) {
+            match word {
+                MossWord::Idle => (),
+                MossWord::UnitFrameHeader => {
+                    debug_assert!(!is_moss_packet);
+                    is_moss_packet = true;
+                    moss_packets.push(MossPacket {
+                        unit_id: *byte & 0x0F,
+                        hits: Vec::new(),
+                    });
+                }
+                MossWord::UnitFrameTrailer => {
+                    debug_assert!(is_moss_packet);
+                    is_moss_packet = false;
+                    last_trailer_idx = i;
+                }
+                MossWord::RegionHeader => {
+                    debug_assert!(is_moss_packet);
+                    current_region = *byte & 0x03;
+                }
+                MossWord::Data0 => {
+                    debug_assert!(is_moss_packet);
+                    moss_packets.last_mut().unwrap().hits.push(MossHit {
+                        region: current_region,            // region id
+                        row: ((*byte & 0x3F) as u16) << 3, // row position [8:3]
+                        column: 0,                         // placeholder
+                    });
+                }
+                MossWord::Data1 => {
+                    debug_assert!(is_moss_packet);
+                    // row position [2:0]
+                    moss_packets
+                        .last_mut()
+                        .unwrap()
+                        .hits
+                        .last_mut()
+                        .unwrap()
+                        .row |= ((*byte & 0x38) >> 3) as u16;
+                    // col position [8:6]
+                    moss_packets
+                        .last_mut()
+                        .unwrap()
+                        .hits
+                        .last_mut()
+                        .unwrap()
+                        .column = ((*byte & 0x07) as u16) << 6;
+                }
+                MossWord::Data2 => {
+                    debug_assert!(is_moss_packet);
+                    moss_packets
+                        .last_mut()
+                        .unwrap()
+                        .hits
+                        .last_mut()
+                        .unwrap()
+                        .column |= (*byte & 0x3F) as u16; // col position [5:0]
+                }
+                MossWord::Delimiter => {
+                    debug_assert!(!is_moss_packet);
+                }
+            }
+        }
+    }
+
+    if moss_packets.is_empty() {
+        Err(PyTypeError::new_err("No MOSS Packets in events"))
+    } else {
+        Ok((moss_packets, last_trailer_idx))
     }
 }
 
@@ -288,6 +380,22 @@ mod tests {
     }
 
     #[test]
+    fn test_decoding_multiple_events_alt() {
+        let events = fake_multiple_events();
+
+        let (packets, unprocessed_data) = decode_multiple_events_alt(&events).unwrap();
+
+        let packet_count = packets.len();
+
+        println!("last trailer at idx: {unprocessed_data}");
+        println!("{packet_count}");
+
+        for p in packets {
+            println!("{p:?}");
+        }
+    }
+
+    #[test]
     fn test_decoding_multiple_events_delimiter() {
         let mut events = fake_multiple_events();
         events.append(&mut vec![0xFA, 0xFA, 0xFA]);
@@ -306,5 +414,26 @@ mod tests {
         for p in moss_packets {
             println!("{p:?}");
         }
+    }
+
+    #[test]
+    #[ignore = "local test file needed"]
+    fn test_read_file_decode() {
+        let time = std::time::Instant::now();
+
+        println!("Reading file...");
+        let f = std::fs::read(std::path::PathBuf::from("../moss_noise.raw")).unwrap();
+        println!(
+            "Read file in: {t:?}. Bytes: {cnt}",
+            t = time.elapsed(),
+            cnt = f.len()
+        );
+
+        println!("Decoding content...");
+        let (p, last_trailer_idx) = decode_multiple_events_alt(&f).unwrap();
+        println!("Decoded in: {t:?}\n", t = time.elapsed());
+
+        println!("Got: {packets}", packets = p.len());
+        println!("Last trailer at index: {last_trailer_idx}");
     }
 }
