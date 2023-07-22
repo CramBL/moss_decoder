34cef146fe61f20784cbdb4a1fd4414c8c56a837
CramBL <marcbkonig@gmail.com>
2023-07-22 12:40:24 +0200

    add moss_protocol module

--- src/moss_protocol.rs
+++ src/moss_protocol.rs
@@ -1,6 +1,10 @@
 use pyo3::prelude::*;
 use std::fmt::{write, Display};
 
+pub use moss_hit::MossHit;
+
+pub mod moss_hit;
+
 pub(crate) enum MossWord {
     Idle,
     UnitFrameHeader,
@@ -76,43 +80,6 @@ impl Display for MossPacket {
     }
 }
 
-#[pyclass(get_all)]
-#[derive(Debug, Default, Clone, Copy, PartialEq)]
-pub struct MossHit {
-    pub region: u8,
-    pub row: u16,
-    pub column: u16,
-}
-
-#[pymethods]
-impl MossHit {
-    #[new]
-    fn new(region: u8, row: u16, column: u16) -> Self {
-        Self {
-            region,
-            row,
-            column,
-        }
-    }
-
-    fn __str__(&self) -> PyResult<String> {
-        Ok(self.to_string())
-    }
-}
-
-impl Display for MossHit {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write(
-            f,
-            format_args!(
-                "reg: {reg} row: {row} col: {col}",
-                reg = self.region,
-                row = self.row,
-                col = self.column,
-            ),
-        )
-    }
-}
 
 #[cfg(test)]
 mod tests {
