a0a7189bbaf1d6e9c07b8a9892fb271716517261
CramBL <marcbkonig@gmail.com>
2023-07-24 14:42:50 +0200

    keep effecient implementations

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@ use moss_protocol::MossWord;
 fn moss_decoder(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(decode_event, m)?)?;
     m.add_function(wrap_pyfunction!(decode_multiple_events, m)?)?;
-    m.add_function(wrap_pyfunction!(decode_multiple_events_alt, m)?)?;
+    m.add_function(wrap_pyfunction!(decode_multiple_events, m)?)?;
 
     m.add_class::<MossHit>()?;
     m.add_class::<MossPacket>()?;
@@ -41,91 +41,12 @@ fn moss_decoder(_py: Python, m: &PyModule) -> PyResult<()> {
 
 const INVALID_NO_HEADER_SEEN: u8 = 0xFF;
 /// Decodes a single MOSS event into a [MossPacket]
-#[pyfunction]
-pub fn decode_event(bytes: &[u8]) -> PyResult<(MossPacket, Vec<u8>)> {
-    let mut hits = Vec::new();
-
-    let mut packet = MossPacket {
-        unit_id: INVALID_NO_HEADER_SEEN, // placeholder
-        hits: Vec::new(),
-    };
-    let mut processed_bytes_idx = 0;
-
-    let mut is_moss_packet = false;
-    let mut current_region: u8 = 0xff; // placeholder
-
-    for (i, byte) in bytes.iter().enumerate() {
-        match MossWord::from_byte(*byte) {
-            MossWord::Idle => (),
-            MossWord::UnitFrameHeader => {
-                debug_assert!(!is_moss_packet);
-                is_moss_packet = true;
-                packet.unit_id = *byte & 0x0F
-            }
-            MossWord::UnitFrameTrailer => {
-                debug_assert!(is_moss_packet);
-                processed_bytes_idx = i + 1;
-                break;
-            }
-            MossWord::RegionHeader => {
-                debug_assert!(is_moss_packet);
-                current_region = *byte & 0x03;
-            }
-            MossWord::Data0 => {
-                debug_assert!(is_moss_packet);
-                hits.push(MossHit {
-                    region: current_region,            // region id
-                    row: ((*byte & 0x3F) as u16) << 3, // row position [8:3]
-                    column: 0,                         // placeholder
-                });
-            }
-            MossWord::Data1 => {
-                debug_assert!(is_moss_packet);
-                // row position [2:0]
-                hits.last_mut().unwrap().row |= ((*byte & 0x38) >> 3) as u16;
-                // col position [8:6]
-                hits.last_mut().unwrap().column = ((*byte & 0x07) as u16) << 6;
-            }
-            MossWord::Data2 => {
-                debug_assert!(is_moss_packet);
-                hits.last_mut().unwrap().column |= (*byte & 0x3F) as u16; // col position [5:0]
-            }
-            MossWord::Delimiter => {
-                debug_assert!(!is_moss_packet);
-            }
-        }
-    }
-
-    if packet.unit_id == INVALID_NO_HEADER_SEEN {
-        return Err(PyTypeError::new_err("No MOSS Packets in event"));
-    }
-    packet.hits.append(&mut hits);
-    let (_processed, unprocessed) = bytes.split_at(processed_bytes_idx);
-    Ok((packet, unprocessed.to_vec()))
-}
-
-/// Decodes multiple MOSS events into a list of [MossPacket]s
-#[pyfunction]
-pub fn decode_multiple_events(mut bytes: Vec<u8>) -> PyResult<Vec<MossPacket>> {
-    let mut moss_packets: Vec<MossPacket> = Vec::new();
-
-    while let Ok((packet, unprocessed_data)) = decode_event(&bytes) {
-        moss_packets.push(packet);
-        bytes = unprocessed_data;
-    }
-
-    if moss_packets.is_empty() {
-        Err(PyTypeError::new_err("No MOSS Packets in events"))
-    } else {
-        Ok(moss_packets)
-    }
-}
 
 const MIN_PREALLOC: usize = 10;
 
 /// Decodes multiple MOSS events into a list of [MossPacket]s
 #[pyfunction]
-pub fn decode_multiple_events_alt(bytes: &[u8]) -> PyResult<(Vec<MossPacket>, usize)> {
+pub fn decode_multiple_events(bytes: &[u8]) -> PyResult<(Vec<MossPacket>, usize)> {
     let byte_cnt = bytes.len();
 
     if byte_cnt < 6 {
@@ -144,7 +65,7 @@ pub fn decode_multiple_events_alt(bytes: &[u8]) -> PyResult<(Vec<MossPacket>, us
 
     let mut last_trailer_idx = 0;
 
-    while let Ok((moss_packet, trailer_idx)) = decode_event_alt(&bytes[last_trailer_idx..]) {
+    while let Ok((moss_packet, trailer_idx)) = decode_event(&bytes[last_trailer_idx..]) {
         moss_packets.push(moss_packet);
         last_trailer_idx += trailer_idx + 1;
     }
@@ -157,7 +78,7 @@ pub fn decode_multiple_events_alt(bytes: &[u8]) -> PyResult<(Vec<MossPacket>, us
 }
 
 #[pyfunction]
-pub fn decode_event_alt(bytes: &[u8]) -> PyResult<(MossPacket, usize)> {
+pub fn decode_event(bytes: &[u8]) -> PyResult<(MossPacket, usize)> {
     let byte_cnt = bytes.len();
 
     if byte_cnt < 6 {
