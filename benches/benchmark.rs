c41febd4830dd41a2baa01fc96c9c8ab2e98ae42
CramBL <marcbkonig@gmail.com>
2023-07-30 08:44:20 +0200

    add benchmark for single event decode

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,6 +1,11 @@
 use criterion::{criterion_group, criterion_main};
 
 mod decode_from_file_bench;
+mod decode_single_event_bench;
 
-criterion_group!(benches, decode_from_file_bench::decode_from_file,);
+criterion_group!(
+    benches,
+    decode_from_file_bench::decode_from_file,
+    decode_single_event_bench::decode_single_event
+);
 criterion_main!(benches);
