2b599beee258665170a4c05de417ca18d2868d29
CramBL <marcbkonig@gmail.com>
2023-07-30 09:55:58 +0200

    add benchmark for decode from file with fsm

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -6,6 +6,7 @@ mod decode_single_event_bench;
 
 criterion_group!(
     benches,
+    decode_from_file_bench::decode_from_file,
     decode_multiple_events_bench::decode_multiple_events,
     decode_single_event_bench::decode_single_event
 );
--- benches/decode_from_file_bench.rs
+++ benches/decode_from_file_bench.rs
@@ -3,21 +3,13 @@ use criterion::Criterion;
 const BENCH_FILE_PATH: &str = "tests/moss_noise.raw";
 
 pub fn decode_from_file(c: &mut Criterion) {
-    let f = std::fs::read(std::path::PathBuf::from(BENCH_FILE_PATH)).unwrap();
-
-    let mut group = c.benchmark_group("decode_from_file");
+    let mut group = c.benchmark_group("decode_multiple_events_bench");
     {
-        group.bench_function("default", |b| {
-            b.iter(|| moss_decoder::decode_multiple_events(&f))
+        group.bench_function("default decode_from_file()", |b| {
+            b.iter(|| moss_decoder::decode_from_file(BENCH_FILE_PATH.into()))
         });
-        group.bench_function("fsm", |b| {
-            b.iter(|| moss_decoder::slower_impls::decode_multiple_events_fsm(&f))
-        });
-        group.bench_function("fsm alternative", |b| {
-            b.iter(|| moss_decoder::slower_impls::decode_multiple_events_fsm_alt(&f))
-        });
-        group.bench_function("fsm iterator", |b| {
-            b.iter(|| moss_decoder::decode_multiple_events_fsm(&f))
+        group.bench_function("fsm iterator decode_from_file_fsm()", |b| {
+            b.iter(|| moss_decoder::decode_from_file_fsm(BENCH_FILE_PATH.into()))
         });
     }
     group.finish();
